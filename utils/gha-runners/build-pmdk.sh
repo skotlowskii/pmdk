231b0cb62c8fcaf045b78ee0526a3d8ca47c43f3
skotlowx <szymonx.kotlowski@intel.com>
2023-03-08 16:30:33 +0100

    PMDK-documentation workflow migration

--- utils/gha-runners/build-pmdk.sh
+++ utils/gha-runners/build-pmdk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2022, Intel Corporation
+# Copyright 2023, Intel Corporation
 
 #
 # build-pmdk.sh - Script for building pmdk project
@@ -8,6 +8,7 @@
 
 FULL_PATH=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 PMDK_PATH="${FULL_PATH}/../.."
+DOC_ARGUMENTS="PMEM2_INSTALL=y PMEMSET_INSTALL=y doc install prefix=${PMDK_PATH}/../install -j"
 
 set -eo pipefail
 
@@ -22,4 +23,10 @@ function build_pmdk {
 	cd ${PMDK_PATH}/ && make test
 }
 
-build_pmdk
+#
+# build_pmdk_documentation -- build pmdk documentation from source
+#
+function build_pmdk_documentation {
+	echo "********** make pmdk_documentation **********"
+	cd ${PMDK_PATH} && make ${DOC_ARGUMENTS}
+}
