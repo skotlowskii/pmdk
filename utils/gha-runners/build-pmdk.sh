5c3c8e8692b8738873d900be494ada0631b2a1ec
skotlowx <szymonx.kotlowski@intel.com>
2023-03-09 09:00:24 +0100

    updates

--- utils/gha-runners/build-pmdk.sh
+++ utils/gha-runners/build-pmdk.sh
@@ -8,7 +8,6 @@
 
 FULL_PATH=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 PMDK_PATH="${FULL_PATH}/../.."
-DOC_ARGUMENTS="PMEM2_INSTALL=y PMEMSET_INSTALL=y doc install prefix=${PMDK_PATH}/../install -j"
 
 set -eo pipefail
 
@@ -23,10 +22,4 @@ function build_pmdk {
 	cd ${PMDK_PATH}/ && make test
 }
 
-#
-# build_pmdk_documentation -- build pmdk documentation from source
-#
-function build_pmdk_documentation {
-	echo "********** make pmdk_documentation **********"
-	cd ${PMDK_PATH} && make ${DOC_ARGUMENTS}
-}
+build_pmdk
