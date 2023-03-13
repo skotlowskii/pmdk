0ce0bf68c08844796d152b9d31b60125777c2e90
skotlowx <szymonx.kotlowski@intel.com>
2023-03-13 12:50:03 +0100

    test

--- utils/gha-runners/build-pmdk.sh
+++ utils/gha-runners/build-pmdk.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2023, Intel Corporation
+# Copyright 2022-2023, Intel Corporation
 
 #
-# build-pmdk.sh - Script for building pmdk project
+# build-pmdk.sh - Scripts for building pmdk project
 #
 
 FULL_PATH=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 PMDK_PATH="${FULL_PATH}/../.."
-DOC_ARGUMENTS="PMEM2_INSTALL=y PMEMSET_INSTALL=y doc install prefix=${PMDK_PATH}/../install -j"
+DOC_ARGUMENTS="PMEM2_INSTALL=y doc install prefix=${PMDK_PATH}/../install -j"
 
 set -eo pipefail
 
 #
-# build_pmdk -- build pmdk from source
+# build_and_test -- build pmdk from source
 #
-function build_pmdk {
+function build_and_test {
 	echo "********** make pmdk **********"
 	cd ${PMDK_PATH} && make clean
 	cd ${PMDK_PATH} && make EXTRA_CFLAGS=-DUSE_VALGRIND
@@ -24,9 +24,14 @@ function build_pmdk {
 }
 
 #
-# build_pmdk_documentation -- build pmdk documentation from source
+# build_documentation -- build documentation from source
 #
-function build_pmdk_documentation {
+function build_documentation {
 	echo "********** make pmdk_documentation **********"
 	cd ${PMDK_PATH} && make ${DOC_ARGUMENTS}
 }
+
+if [ "$#" -eq 1 ]; then
+  build_method=$1
+  ${build_method}
+fi
