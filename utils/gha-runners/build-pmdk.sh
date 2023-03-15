741a7ae6d3ef8d32ca05fccf4ea9c45f0dec1351
skotlowx <szymonx.kotlowski@intel.com>
2023-03-15 08:43:32 +0100

    common: import tests from pmdk-tests (src/tests/scripts/split_manpage)

--- utils/gha-runners/build-pmdk.sh
+++ utils/gha-runners/build-pmdk.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2022-2023, Intel Corporation
+# Copyright 2022, Intel Corporation
 
 #
-# build-pmdk.sh - Scripts for building pmdk project
+# build-pmdk.sh - Script for building pmdk project
 #
 
 FULL_PATH=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
@@ -12,9 +12,9 @@ PMDK_PATH="${FULL_PATH}/../.."
 set -eo pipefail
 
 #
-# build_and_test -- build pmdk from source
+# build_pmdk -- build pmdk from source
 #
-function build_and_test {
+function build_pmdk {
 	echo "********** make pmdk **********"
 	cd ${PMDK_PATH} && make clean
 	cd ${PMDK_PATH} && make EXTRA_CFLAGS=-DUSE_VALGRIND
@@ -22,4 +22,4 @@ function build_and_test {
 	cd ${PMDK_PATH}/ && make test
 }
 
-build_and_test
+build_pmdk
